382c75248eae30ae53edfc8f71883ec71d3d77fc
Gus Diaz <adrian.gustavo.diaz@gmail.com>
2018-03-18 10:11:25 -0600

    Generate html and preview on post save

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+require 'redcarpet'
+require 'redcarpet/render_strip'
+
 class Post < ApplicationRecord
+
+  def save
+    generate_html_and_preview
+    super
+  end
+
+  def update(attribute_names)
+    generate_html_and_preview
+    super(attribute_names)
+  end
+
+  private
+    def generate_html_and_preview
+      text_only    = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
+      markdown     = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, strikethrough: true, underline: true, highlight: true, quote: true)
+      self.html    = markdown.render(self.content)
+      self.preview = text_only.render(self.content).to(300)
+    end
 end
